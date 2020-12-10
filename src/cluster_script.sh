4ea01be10ce82df2279117d6852ac38869d19dbe
Patrick <patricksinclair@hotmail.co.uk>
2020-12-10 14:08:48 +0000

    many runs s1

--- src/cluster_script.sh
+++ src/cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#$ -N bftt_t1_powerLaw-NvsT-nThresh-1.35# job name
+#$ -N bftt_t1_powerLaw-NvsT-nThresh-0.825# job name
 #$ -V           # use all shell environment variables
 #$ -cwd         # run job in working directory
 # #$ -j y         # merge stdout and stderr to one file
