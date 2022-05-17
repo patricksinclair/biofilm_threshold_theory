840255e0de58927d5bd4d3f768a7bf9bc48e93ea
patricksinclair <pcsinclair@outlook.com>
2022-05-17 11:28:38 +0100

    small tau

--- out/production/biofilm_threshold_theory/cluster_script.sh
+++ out/production/biofilm_threshold_theory/cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#$ -N bftt_4c1_solo # job name
+#$ -N bftt_t1_powerLaw-NvsT-nThresh-0.45-session2# job name
 #$ -V           # use all shell environment variables
 #$ -cwd         # run job in working directory
 # #$ -j y         # merge stdout and stderr to one file
@@ -13,7 +13,7 @@
 # Choose a parallel environment:
 # Check options with "qconf -spl"
 # Check details with "qconf -sp <pe-name>"
-#$ -pe mpi 10       # asks for n processors for an mpi job
+#$ -pe mpi 20       # asks for n processors for an mpi job
 #$ -l h_vmem=2G     # asks for n Gb of memory
 # Send mail at submission and completion of script
 #$ -m be
@@ -41,6 +41,6 @@ time {
 # put the usual mpirun command here
 # the shell variable $NSLOTS is the number of cores you asked for above
 
-java -cp ".:java_jars/*" MultispeciesMain $NSLOTS > bft_log.out
+java -cp ".:java_jars/*" MultispeciesMain $NSLOTS > bftt_log.out
 
 }
