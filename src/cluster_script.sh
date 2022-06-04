d1a60b22734728d8282eeb34bbd614c4713129c5
patricksinclair <pcsinclair@outlook.com>
2022-06-04 14:29:25 +0100

    changed job name

--- src/cluster_script.sh
+++ src/cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#$ -N bftt_v2_figS5_intermediate # job name
+#$ -N bftt_v2_K_1000_rIm0.7 # job name
 #$ -V           # use all shell environment variables
 #$ -cwd         # run job in working directory
 # #$ -j y         # merge stdout and stderr to one file
